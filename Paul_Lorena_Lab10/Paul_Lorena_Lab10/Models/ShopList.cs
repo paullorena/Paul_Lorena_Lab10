01173aaf7de9140133098183245403be8628a8d2
Lorena Amalia Paul <Paul.Va.Lorena@student.utcluj.ro>
2021-12-09 17:57:29 +0200

    Add project files.

---
+++ Paul_Lorena_Lab10/Paul_Lorena_Lab10/Models/ShopList.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using SQLite;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Paul_Lorena_Lab10.Models
+{
+    public class ShopList
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+        public string Description { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
